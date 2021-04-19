7d0d3445c8c95e8d87cf65235f2b6d3ce3871543
chandu0101 <chandu.csu2010@gmail.com>
2021-04-19 09:41:12 +0530

    update

--- packages/dstore_generator/lib/src/graphql/globals.dart
+++ packages/dstore_generator/lib/src/graphql/globals.dart
@@ -1,3 +1,4 @@
+import 'package:dstore_annotation/dstore_annotation.dart';
 import 'package:gql/schema.dart';
 
-final graphqlSchemaMap = <String, GraphQLSchema>{};
+final graphqlSchemaMap = <GraphqlApi, GraphQLSchema>{};
