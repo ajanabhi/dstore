72fe5aecb3d7aeb6c285a1e2d15a79cd977f166d
chandu0101 <chandu.csu2010@gmail.com>
2021-04-21 22:44:32 +0530

    update

--- packages/dstore_generator/lib/src/graphql/ops/dart_dsl.dart
+++ packages/dstore_generator/lib/src/graphql/ops/dart_dsl.dart
@@ -1,9 +1,10 @@
 class Query {
-  late String hello;
-  late String hello1;
-  late String ping;
-  late Todo todo;
-  late Todo users;
+  late Query hello;
+  late Query hello1;
+  late Query ping;
+  Query todo(Todo t) => throw Error();
+  Query users(Person t) => throw Error();
+  Query hellou(HelloU t) => throw Error();
 }
 
 class Todo {
@@ -13,9 +14,11 @@ class Todo {
 
 class Person {
   late Person name;
-  late String screen;
-  late String screens;
-  late String tags;
+  late Person screen;
+  late Person screens;
+  late Person tags;
+  Person hello(HelloU u) => throw Error();
+  Person helloa(HelloU u) => throw Error();
 }
 
 class Hello1 {
@@ -34,6 +37,18 @@ class HelloU {
   HelloU unionfrag_Hello2(Hello2 h2) => throw Error();
 }
 
-class Country {
-  // Country get
+class Address {
+  late Address street;
+  late Address zip;
+  Address country(Country c) => throw Error();
 }
+
+class Country {
+  late Country code;
+  late Country name;
+}
+
+final q = Query().todo(Todo().text).users(Person()
+    .name
+    .tags
+    .hello(HelloU().d__typename.unionfrag_Hello1(Hello1().name)));
