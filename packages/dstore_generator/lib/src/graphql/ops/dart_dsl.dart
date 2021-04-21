73d1236cc3101425de6366360210cbcca42d8e77
chandu0101 <chandu.csu2010@gmail.com>
2021-04-21 07:42:47 +0530

    update

---
+++ packages/dstore_generator/lib/src/graphql/ops/dart_dsl.dart
@@ -0,0 +1,32 @@
+class Query {
+  String get hello => throw Error();
+  String get hello1 => throw Error();
+  String get ping => throw Error();
+  Todo get todo => throw Error();
+  Todo get users => throw Error();
+}
+
+class Todo {
+  Todo get text => throw Error();
+}
+
+class Person {
+  Person get name => throw Error();
+  String get screen => throw Error();
+  String get screens => throw Error();
+  String get tags => throw Error();
+}
+
+class Hello1 {
+  Hello1 get name => throw Error();
+  Hello1 get one => throw Error();
+}
+
+class Hello2 {
+  Hello2 get name => throw Error();
+  Hello2 get two => throw Error();
+}
+
+class HelloU {
+  HelloU get d__typename => throw Error();
+}
