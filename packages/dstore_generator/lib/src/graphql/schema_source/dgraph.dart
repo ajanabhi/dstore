371848121fc36fec1c196efe3838ca1f86e3878e
chandu0101 <chandu.csu2010@gmail.com>
2021-04-28 08:37:04 +0530

    update

--- packages/dstore_generator/lib/src/graphql/schema_source/dgraph.dart
+++ packages/dstore_generator/lib/src/graphql/schema_source/dgraph.dart
@@ -1,12 +1,18 @@
+import 'package:analyzer/dart/constant/value.dart';
 import 'package:analyzer/dart/element/element.dart';
 import 'package:dstore_generator/src/utils/utils.dart';
 import 'package:dstore_dgraph/dgraph.dart';
 
-String getDGraphFieldAnnotations({required FieldElement fe}) {
+String getDGraphFieldAnnotations({required FieldElement element}) {
+  final annotations = <String>[];
+  final hasI = getHasInverseDirective(element);
+  if (hasI != null) {
+    annotations.add("hasInverse(field: ${hasI.field})");
+  }
   return "";
 }
 
-hasInverse? getHasInverse(Element element) {
+hasInverse? getHasInverseDirective(Element element) {
   final annot = element.annotationFromType(hasInverse)?.computeConstantValue();
   if (annot != null) {
     final field = annot.getField("field")?.toStringValue();
@@ -14,9 +20,89 @@ hasInverse? getHasInverse(Element element) {
   }
 }
 
-search? getSearch(Element element) {
+search? getSearchDirective(Element element) {
   final annot = element.annotationFromType(search)?.computeConstantValue();
   if (annot != null) {
-    final field = annot.getField("by")?.toListValue();
+    final field = annot
+        .getField("by")
+        ?.toListValue()
+        ?.map((e) => e.getEnumValue(DgraphIndex.values)!)
+        .toList();
+    return search(field);
+  }
+}
+
+dgraph? getDGraphDirective(Element element) {
+  final annot = element.annotationFromType(dgraph)?.computeConstantValue();
+  if (annot != null) {
+    final type = annot.getField("type")?.toStringValue();
+    final pred = annot.getField("pred")?.toStringValue();
+    return dgraph(type: type, pred: pred);
+  }
+}
+
+id? getIdDirective(Element element) {
+  final annot = element.annotationFromType(id)?.computeConstantValue();
+  if (annot != null) {
+    return id();
+  }
+}
+
+custom? getCustomDirective(Element element) {
+  final annot = element.annotationFromType(id)?.computeConstantValue();
+  if (annot != null) {
+    final http = getCustomHttp(annot.getField("http"));
+    final dql = annot.getField("dql")?.toStringValue();
+    return custom(http: http, dql: dql);
+  }
+}
+
+cascade? getCascadeDirective(Element element) {
+  final annot = element.annotationFromType(cascade)?.computeConstantValue();
+  if (annot != null) {
+    final fields = annot
+        .getField("fields")
+        ?.toListValue()
+        ?.map((e) => e.toStringValue()!)
+        .toList();
+    return cascade(fields);
+  }
+}
+
+lambda? getLambdaDirective(Element element) {
+  final annot = element.annotationFromType(cascade)?.computeConstantValue();
+  if (annot != null) {
+    return lambda();
+  }
+}
+
+CustomHTTP? getCustomHttp(DartObject? obj) {
+  if (obj != null) {
+    final url = obj.getField("url")?.toStringValue();
+    final method = obj.getEnumField("method", HTTPMethod.values);
+    final mode = obj.getEnumField("mode", Mode.values);
+    final body = obj.getField("body")?.toStringValue();
+    final graphql = obj.getField("graphql")?.toStringValue();
+    final skipIntrospection = obj.getField("skipIntrospection")?.toBoolValue();
+    final forwardHeaders = obj
+        .getField("forwardHeaders")
+        ?.toListValue()
+        ?.map((e) => e.toStringValue()!)
+        .toList();
+
+    final introspectionHeaders = obj
+        .getField("introspectionHeaders")
+        ?.toListValue()
+        ?.map((e) => e.toStringValue()!)
+        .toList();
+    return CustomHTTP(
+        url: url!,
+        method: method!,
+        mode: mode,
+        body: body,
+        graphql: graphql,
+        forwardHeaders: forwardHeaders,
+        skipIntrospection: skipIntrospection,
+        introspectionHeaders: introspectionHeaders);
   }
 }
