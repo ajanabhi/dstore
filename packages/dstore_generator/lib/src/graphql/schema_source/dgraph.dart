c5d71de07642b0b64e360a1728d77ef061b3e953
chandu0101 <chandu.csu2010@gmail.com>
2021-04-28 06:24:40 +0530

    update

--- packages/dstore_generator/lib/src/graphql/schema_source/dgraph.dart
+++ packages/dstore_generator/lib/src/graphql/schema_source/dgraph.dart
@@ -1,5 +1,22 @@
 import 'package:analyzer/dart/element/element.dart';
+import 'package:dstore_generator/src/utils/utils.dart';
+import 'package:dstore_dgraph/dgraph.dart';
 
 String getDGraphFieldAnnotations({required FieldElement fe}) {
   return "";
+}
+
+hasInverse? getHasInverse(Element element) {
+  final annot = element.annotationFromType(hasInverse)?.computeConstantValue();
+  if (annot != null) {
+    final field = annot.getField("field")?.toStringValue();
+    return hasInverse(field!);
+  }
+}
+
+search? getSearch(Element element) {
+  final annot = element.annotationFromType(search)?.computeConstantValue();
+  if (annot != null) {
+    final field = annot.getField("by")?.toListValue();
+  }
 }
