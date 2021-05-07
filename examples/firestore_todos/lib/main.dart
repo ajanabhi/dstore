4c8db7cd60552e3e3e8ef5987894138d9655c161
chandu0101 <chandu.csu2010@gmail.com>
2021-05-07 14:43:11 +0530

    update

--- examples/firestore_todos/lib/main.dart
+++ examples/firestore_todos/lib/main.dart
@@ -1,12 +1,61 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello FS"),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("Hello FireStore"),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+                onPressed: () async {
+                  try {
+                    print("Hello");
+                    final collRef =
+                        FirebaseFirestore.instance.collection("rooms");
+                    final data = await collRef.doc("roomA").get();
+                    print("done ${data.data()} ${data.id}");
+                  } catch (e) {
+                    print("Error $e");
+                  }
+                },
+                child: Text("Get It"))
+          ],
+        ),
+      ),
+    );
+  }
+
+  void addData(CollectionReference collRef) async {
+    await collRef
+        .doc("roomA")
+        .collection("messages")
+        .doc("m1")
+        .set(<String, dynamic>{"name": "n1"});
   }
 }
