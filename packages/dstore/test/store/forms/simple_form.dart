a0b05be098249145b4dfa16fc8b084191f7d4cc0
chandu0101 <chandu.csu2010@gmail.com>
2021-05-25 18:53:06 +0530

    update

--- packages/dstore/test/store/forms/simple_form.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dstore/dstore.dart';
-
-import "package:meta/meta.dart";
-part "simple_form.dstore.dart";
-
-@FormModel()
-class $_SimpleForm {
-  String name = "";
-}
--- packages/dstore/test/store/pstates/sample2/sample2.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dstore/dstore.dart';
-import "package:meta/meta.dart";
-part "sample2.ps.dstore.dart";
-
-@PState()
-class $_Sample2 {
-  final String name = "name2";
-}
